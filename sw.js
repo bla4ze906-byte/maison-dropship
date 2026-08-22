597cbbfc2382edcfc5fb5a4e50e992396a24edd9
bla4ze906-byte <bla4ze906@gmail.com>
2026-08-22 11:52:07 +0200

    Convert app to PWA with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'maison-dropship-v1';
+const urlsToCache = [
+  '/',
+  '/ecommerce.html',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.log('Cache failed:', error);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          return caches.match('/ecommerce.html');
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
